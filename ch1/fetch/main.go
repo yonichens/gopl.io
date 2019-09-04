d0851422ce365df588d166ae80ae5e5ca4ca7edc
yonichens <yonichens@hotmail.com>
2019-09-04 10:30:20 +0800

    练习	1.8：strings.HasPrefix
    练习	1.8：修改fetch这个范例，如果输入的url参数没有 http:// 前缀的话，为这个url加上该前缀。你可能会用到strings.HasPrefix这个函数。

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -10,6 +10,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
+
 	//"io/ioutil"
 	"net/http"
 	"os"
@@ -17,6 +19,9 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
