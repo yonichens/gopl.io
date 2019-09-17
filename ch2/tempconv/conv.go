613552d021a3e5326cfed072f0f445ed7cf3497b
yonichens <yonichens@hotmail.com>
2019-09-17 15:40:45 +0800

    练习	2.1：类型、包和文件
    练习	2.1：向tempconv包添加类型、常量和函数用来处理Kelvin绝对温度的转换，Kelvin绝 对零度是−273.15°C，Kelvin绝对温度1K和摄氏度1°C的单位间隔是一样的。

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -13,4 +13,8 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+func KToC(k Kelvin) Celsius { return Celsius(k + 273.15) }
+
+func CToK(c Celsius) Kelvin { return Kelvin(c - 273.15) }
+
 //!-
--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -10,14 +10,18 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
+	AbsoluteZeroK Kelvin = 0
+
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string { return fmt.Sprintf("%gK", k) }
 
 //!-
